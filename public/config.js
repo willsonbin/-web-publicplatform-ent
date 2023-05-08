fab30b66c5da853319b53a8fbfbb77046995a9a2
HoBan <willsonbin6@hotmail.com>
2023-05-08 17:03:50 +0800

    0.1 修改后台服务器地址

--- public/config.js
+++ public/config.js
@@ -4,6 +4,7 @@ window.config = {
   },
   // traceUrl: 'http://192.168.1.155:5300',
   // traceUrl: 'https://ent-platform.qccvas.com/enterprise-gateway', //福州
-  traceUrl: 'http://192.168.1.245:5300',
+  // traceUrl: 'http://192.168.1.245:5300',
+  traceUrl: 'https://pts.caiqgba.org.cn/enterprise-gateway',
   remoteDebugg: false // true通过权限控制
 }
